9fbc394b437702472c42c34d2adc3c3fe6062d0e
G90SG <71718345+G90SG@users.noreply.github.com>
2021-02-22 12:08:20 +0000

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,22 +1,22 @@
-#creating number variable (integer)
+# Creating number variable and declaring as integer
 number = 0 
-#creating while loop for while the number is less than 100
+# Creating while loop for while the number is less than 100
 while number < 100 do 
-  #adding 1 to the number
+  # Using 'do' to add 1 to the number at each run of the while loop
   number = number + 1 
-  # using if statement and modulo to find out if the number is divisible by 3 and printing Fizz
+  # Using IF statement and Modulo to find out if the number is divisible by 3 and printing 'Fizz'
   if (number % 3 == 0) 
   puts "Fizz"
-  #using elsif and modulo to find out if the number is divisible by 5 and printing Buss
+  # Using ELSIF and Modulo to find out if the number is divisible by 5 and printing Buss
     elsif (number % 5 == 0) 
       puts "Buzz"
-  #using elsif to find out if divisible by both, printing FizzBuzz
+  # Using ELSIF and 'and' to find out if divisible by both, printing FizzBuzz
     elsif (number % 3 == 0) and (number % 5 == 0) 
       puts "FizzBuzz"
-  #else, if not divisible by any of the above, just printing the number 
+  # ELSE if not divisible by any of the above, just printing the number 
   else 
     puts "#{number}"
-  #ending IF statement
+  # Ending IF statement
   end
-  #ending While Loop
+  # Ending While Loop
 end
